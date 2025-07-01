aa67a627d6f0fe393244d16d401cdcd93e6e99ac
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-06-30 22:17:50 -0300

    CHORE: commit inicial

---
+++ helloworld/hello_world_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+func TestHelloWorld(t *testing.T) {
+	expected := "Hello, World!"
+	got := HelloWorld()
+
+	if expected != got {
+		t.Errorf("HelloWorld() = %q; want %q", got, expected)
+	}
+}
