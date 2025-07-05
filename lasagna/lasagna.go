4f647e28fc590c88dc51e66849c1c28a68d85b45
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-07-05 20:42:26 -0300

    FEAT: adiciona funções de resolução do exercício

--- lasagna/lasagna.go
+++ lasagna/lasagna.go
@@ -1,1 +1,15 @@
 package lasagna
+
+const ovenTime = 40
+
+func RemaningOvenTime(actualMinutesInOven int) int {
+	return ovenTime - actualMinutesInOven
+}
+
+func PreparationTime(numberOfLayers int) int {
+	return numberOfLayers * 2
+}
+
+func ElepsedTime(numberOfLayers, actualMinutesInOven int) int {
+	return (numberOfLayers * 2) + actualMinutesInOven
+}
