8a1a7dc239233b772277236323bfdf325630a7bd
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-07-05 21:52:52 -0300

    FEAT: adiciona funções de resolução do exercício

--- annalyn/annalyns_infiltration.go
+++ annalyn/annalyns_infiltration.go
@@ -1,1 +1,29 @@
 package annalyn
+
+func CanFastAttack(knightIsAwake bool) bool {
+	return !knightIsAwake
+}
+
+func CanSpy(knightIsAwake, archerIsAwake, prisonerIsAwake bool) bool {
+	if knightIsAwake || archerIsAwake || prisonerIsAwake {
+		return true
+	}
+
+	return false
+}
+
+func CanSignalPrisioner(archerIsAwake, prisonerIsAwake bool) bool {
+	if archerIsAwake && prisonerIsAwake {
+		return false
+	}
+
+	return true
+}
+
+func CanFreePrisionar(knightIsAwake, archerIsAwake, prisonerIsAwake, petDogIsPresent bool) bool {
+	if petDogIsPresent && !archerIsAwake || !petDogIsPresent && prisonerIsAwake && !archerIsAwake && !knightIsAwake {
+		return true
+	}
+
+	return false
+}
