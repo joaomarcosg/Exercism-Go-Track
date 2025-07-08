294b98fbd6f3ede2fc793411c7441d5eaa08e8f7
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-07-07 21:20:41 -0300

    TEST: adiciona teste para função HappyBirthday

--- partyrobot/party_robot_test.go
+++ partyrobot/party_robot_test.go
@@ -12,3 +12,12 @@ func TestWelcome(t *testing.T) {
 		t.Errorf("Welcome() = %s, want %s", got, expected)
 	}
 }
+
+func TestHappyBirthday(t *testing.T) {
+	expected := "Happy birthday Frank! You are now 58 years old!"
+	got := HappyBirthday("Frank", 58)
+
+	if got != expected {
+		t.Errorf("HappyBirthday() = %s, want %s", got, expected)
+	}
+}
