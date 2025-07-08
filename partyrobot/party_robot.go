7f899f0035e489a27a7fe088a9d51f9b6a104c2a
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-07-07 21:53:06 -0300

    TEST: adiciona teste para AssignTable

--- partyrobot/party_robot.go
+++ partyrobot/party_robot.go
@@ -11,6 +11,6 @@ func HappyBirthday(name string, age int) string {
 }
 
 func AssignTable(name string, table int, neighbor, direction string, distance float64) string {
-	return fmt.Sprintf("Welcome to my party %s!You have been assigned to table %03d.Your table is %s, exacty %1.f meters from here. You will be sitting next to %s",
+	return fmt.Sprintf("Welcome to my party %s!You have been assigned to table %03d.Your table is %s, exactly %.1f meters from here. You will be sitting next to %s",
 		name, table, direction, distance, neighbor)
 }
--- partyrobot/party_robot_test.go
+++ partyrobot/party_robot_test.go
@@ -21,3 +21,12 @@ func TestHappyBirthday(t *testing.T) {
 		t.Errorf("HappyBirthday() = %s, want %s", got, expected)
 	}
 }
+
+func TestAssignTable(t *testing.T) {
+	expected := "Welcome to my party Christiane!You have been assigned to table 027.Your table is on the left, exactly 23.8 meters from here. You will be sitting next to Frank"
+	got := AssignTable("Christiane", 27, "Frank", "on the left", 23.7834298)
+
+	if got != expected {
+		t.Errorf("AssingTable() = %s, want %s", got, expected)
+	}
+}
