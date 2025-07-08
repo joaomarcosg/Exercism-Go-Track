6d7d892f031327bd51dcfb536b29aca1d414a00d
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-07-07 21:17:43 -0300

    FEAT: adiciona função HappyBirthday

--- partyrobot/party_robot.go
+++ partyrobot/party_robot.go
@@ -5,3 +5,7 @@ import "fmt"
 func Welcome(name string) string {
 	return fmt.Sprintf("Welcome to my party %s", name)
 }
+
+func HappyBirthday(name string, age int) string {
+	return fmt.Sprintf("Happy birthday %s! You are now %d years old!", name, age)
+}
