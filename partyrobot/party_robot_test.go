242f5fc9add5118751f7e27b47655fe5f02fc67b
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-07-08 21:13:29 -0300

    FIX: corrige funções

--- partyrobot/party_robot_test.go
+++ partyrobot/party_robot_test.go
@@ -9,6 +9,12 @@ type Struct struct {
 	want string
 }
 
+type Birthday struct {
+	name string
+	age  int
+	want string
+}
+
 func TestWelcome(t *testing.T) {
 
 	tt := Struct{
@@ -20,22 +26,4 @@ func TestWelcome(t *testing.T) {
 		t.Errorf("Welcome(%s) = %s, want %s", tt.name, got, tt.want)
 	}
 
-}
-
-func TestHappyBirthday(t *testing.T) {
-	expected := "Happy birthday Frank! You are now 58 years old!"
-	got := HappyBirthday("Frank", 58)
-
-	if got != expected {
-		t.Errorf("HappyBirthday() = %s, want %s", got, expected)
-	}
-}
-
-func TestAssignTable(t *testing.T) {
-	expected := "Welcome to my party Christiane!You have been assigned to table 027.Your table is on the left, exactly 23.8 meters from here. You will be sitting next to Frank"
-	got := AssignTable("Christiane", 27, "Frank", "on the left", 23.7834298)
-
-	if got != expected {
-		t.Errorf("AssingTable() = %s, want %s", got, expected)
-	}
 }
